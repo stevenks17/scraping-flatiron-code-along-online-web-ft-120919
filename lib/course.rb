7775aede5eca91bac20ed29f4397af64c87be665
Steven K <stevenks17@gmail.com>
2020-02-20 16:36:10 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,5 +1,17 @@
-Class Course
+class Course
+  attr_accessor :title, :schedule, :description
 
-attr_accessor :title
+  @@all = []
 
+  def initialize
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
